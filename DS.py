4537d0e205dd5adca2dd726ae507b2c7d1bf164c
omkar8087 <90601006+omkar8087@users.noreply.github.com>
2022-09-16 20:57:02 +0530

    Add files via upload

---
+++ DS.py
@@ -0,0 +1,14 @@
+# Python code to demonstrate the working of
+# mean()
+ 
+# importing statistics to handle statistical
+# operations
+import statistics
+ 
+# initializing list
+li = [1, 2, 3, 3, 2, 2, 2, 1]
+ 
+# using mean() to calculate average of list
+# elements
+print ("The average of list values is : ",end="")
+print (statistics.mean(li))
